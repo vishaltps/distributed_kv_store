190c039eb700782764856ce8ca3316c9811eac44
Vishal Sadriya <vishal.sadriya@mondu.ai>
2023-02-14 19:30:36 +0100

    added get method and fix ttl issue

--- app/controllers/api/v1/key_values_controller.rb
+++ app/controllers/api/v1/key_values_controller.rb
@@ -7,9 +7,9 @@ module Api
 				render json: result
 			end
 
-			def create
+			def update
 				begin
-					record = ActiveRecord::Base.connection.execute("INSERT INTO stores (key, values, ttl) VALUES ('#{permitted_params[:key]}', '#{permitted_params[:values]}', extract(epoch from ((now() at time zone 'UTC') - interval '2 hour'))) ON CONFLICT(key) DO UPDATE SET values = '#{permitted_params[:values]}';")
+					record = ActiveRecord::Base.connection.execute("INSERT INTO stores (key, values, ttl) VALUES ('#{permitted_params[:key]}', '#{permitted_params[:values]}', extract(epoch from ((now() at time zone 'UTC')  + interval '2 hour'))) ON CONFLICT(key) DO UPDATE SET values = '#{permitted_params[:values]}';")
 					render json: { data: record.first, message: 'record created successfully'}
 				rescue StandardError => e
 					render json: { error_messages: "Record not created, error: #{e.message}"}, status: 500
@@ -18,14 +18,25 @@ module Api
 
 			def destroy
 				begin
-					# result = ActiveRecord::Base.connection.execute("SELECT * from stores WHERE key = '#{params[:key]}' AND ttl > extract(epoch from ((now() at time zone 'UTC')))")
-					result = ActiveRecord::Base.connection.execute("UPDATE stores SET ttl = -1 WHERE key = '#{params[:key]}'")
+					result = ActiveRecord::Base.connection.execute("UPDATE stores SET ttl = -1 WHERE key = '#{params[:key]}' AND ttl > extract(epoch from ((now() at time zone 'UTC')))")
 					render json: { message: 'record removed successfully'}
 				rescue StandardError => e
 					render json: { error_messages: "Record not deleted, error: #{e.message}"}, status: 500
 				end
 			end
 
+			def show
+				begin
+					result = ActiveRecord::Base.connection.execute("SELECT * from stores WHERE key = '#{params[:key]}' AND ttl > extract(epoch from ((now() at time zone 'UTC')))")
+					if result.cmd_tuples.positive?
+						render json: result.first
+					else
+						render json: { error_messages: 'Record not found' }, status: 404
+					end
+				rescue StandardError => e
+					render json: { error_messages: "Record couldn't fetch, error: #{e.message}"}, status: 500
+				end
+			end
 
 			def permitted_params
 				params.permit(:key, :values)
